c1751d815907b4721e386012708290074db050ad
Alexander Helbok <alexander.helbok@hotmail.com>
2022-03-23 09:18:10 +0100

    5.03

--- 5.py
+++ 5.py
@@ -2,6 +2,12 @@ import xlrd
 import matplotlib.pyplot as plt
 from numpy import exp
 import numpy as np
+from statistics import fmean, stdev
+
+
+def poisson(mean, x):
+    sum = exp(-mean)*mean**x/np.math.factorial(x)
+    return sum
 
 
 # ========== 1 ===========
@@ -10,33 +16,41 @@ infile = "datensaetze.xlsx"
 wb = xlrd.open_workbook(infile)
 sheet = wb.sheet_by_index(2)
 
-Data1 = []
-Data2 = []
-Time = []
+Data1, Data2, Time = [], [], []
 
 for i in range(1, sheet.nrows):
     # print(sheet.cell_value(i, 1))
-    Time.append(i)
     Data1.append(sheet.cell_value(i, 1))
     Data2.append(sheet.cell_value(i, 2))
+
+Data2.sort()
+Data1.sort()
+mean1 = fmean(Data1)
+mean2 = fmean(Data2)
+
+Poisson1, Poisson2 = [], []
+
+for i in range(0, 60):
+    Poisson1.append(poisson(mean1, i)*100)
+    Poisson2.append(poisson(mean2, i)*100)
+    Time.append(i)
 
 print(Data1)
 print(Data2)
 
-plt.hist(Data1, density=False, bins=100)  # density=False would make counts
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.hist(Data1, density=False, bins=60)  # density=False would make counts
+ax1.plot(Time, Poisson1)
 # plt.bar(Time, Data2, color="green")
-plt.ylabel('#')
-plt.xlabel('Data')
+# ax1.ylabel('#')
+# ax1.xlabel('Data')
+ax2.hist(Data2, density=False, bins=60)  # density=False would make counts
+ax2.plot(Time, Poisson2)
 # plt.savefig('Data1.png')
-# plt.show()
+plt.show()
 
 
 # =========== 2 ===========
-def poisson(mean, x):
-    sum = exp(-mean)*mean**x/np.math.factorial(x)
-    return sum
-
-
 mean1 = 1.657
 std1 = mean1**0.5
 mean2 = 1.209
@@ -44,7 +58,7 @@ std2 = mean2**0.5
 
 p1 = round(poisson(mean1, 0) * poisson(mean2, 0), 3)
 p2 = round(poisson(mean1, 3) * poisson(mean2, 1), 3)
-p3 = p4 = p5 = 0
+p3, p4, p5 = 0, 0, 0
 
 for i in range(0, 10):
     p3 += poisson(mean1, i)*poisson(mean2, i)
