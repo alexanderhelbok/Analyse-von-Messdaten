3fa1fa23557fa2417cc4f1b9de25ad099c65c6c2
Alexander Helbok <alexander.helbok@hotmail.com>
2022-05-09 17:17:06 +0200

    12

--- 12.py
+++ 12.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xlrd
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 from scipy.optimize import curve_fit
 
@@ -35,7 +36,7 @@ def test2(x, a, b):
     return a * (x - xsp) + b
 
 
-param, param_cov = curve_fit(test1, xi, y, sigma=alpha)
+param, param_cov = curve_fit(test1, xi, y, sigma=alpha, absolute_sigma=True)
 
 print('a\u0305 = {}, b\u0305 = {}'.format(param[0], param[1]))
 print(np.corrcoef(xi, y))
@@ -46,22 +47,16 @@ print('A\u0305 = {}, B\u0305 = {}'.format(param2[0], param2[1]))
 print(param_cov)
 print(param_cov2)
 
-
-
-# plt.errorbar(xi, y, yerr=alpha, fmt='.k', capsize=3, label='Data')
-# plt.plot(xi, test1(xi, param[0], param[1]), '--', color='blue', label='Normal Fit')
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.legend()
-# plt.show()
-
 # ======== 2 ===============
-print(chisq(y, test1(xi, param[0], param[1]), alpha))
-ai, bi = np.linspace(1.9, 2, 21), np.linspace(10, 11, 51)
+ai, bi = np.linspace(1.85, 2.05, 50), np.linspace(9, 13, 50)
 fun_map = np.empty((ai.size, bi.size))
 for i in range(ai.size):
     for j in range(bi.size):
         fun_map[i, j] = chisq(y, test1(xi, ai[i], bi[j]), alpha) - chisq(y, test1(xi, param[0], param[1]), alpha)
 
-ax = sns.heatmap(fun_map)
+
+z = pd.DataFrame(fun_map)
+z.columns = np.round((bi - param[1])/0.75, 0)
+z.index = np.round((param[0] - ai)/0.05, 0)
+ax = sns.heatmap(z, xticklabels=12, yticklabels=12, vmin=0, vmax=5)
 plt.show()
