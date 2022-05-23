7d83eaebed2333ae559f76827f662ebb607f86a2
Alexander Helbok <alexander.helbok@hotmail.com>
2022-05-23 15:25:58 +0200

    16.

--- 16.py
+++ 16.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import uncertainties as unc
 import uncertainties.unumpy as unumpy
 from scipy.optimize import curve_fit
+from scipy import stats
 from main import *
 # ========= 1 =========
 infile = "datensaetze.xlsx"
@@ -16,15 +17,39 @@ sheet = wb.sheet_by_index(17)
 xi = np.linspace(-10, 10, 41)
 temp1, temp2, temp3 = [], [], []
 for i in range(1, sheet.nrows):
-    print(sheet.cell_value(i, 0))
+    # print(sheet.cell_value(i, 0))
     temp1.append(sheet.cell_value(i, 1))
     temp2.append(sheet.cell_value(i, 2))
     # temp3.append(sheet.cell_value(i, 3))
 
 y, alpha = np.array(temp1), np.array(temp2)
-chi = []
+chi, rchi = [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]
 
 
-for i in range(2, 7):
-    param, param_cov = curve_fit(test(grad=i), xi, y)
-    chi[i-2] = chisq(y, test(xi, param[0], param[1]), alpha)
+param, param_cov = curve_fit(test2, xi, y)
+chi[0] = chisq(y, test2(xi, param[0], param[1]), alpha)/(len(xi) - 2)
+
+plt.errorbar(xi, y, yerr=alpha, fmt='.k', capsize=3, label='Data')
+plt.plot(xi, test2(xi, param[0], param[1]), '--', color='blue', label='Normal Fit')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+param, param_cov = curve_fit(test3, xi, y)
+chi[1] = chisq(y, test3(xi, param[0], param[1], param[2]), alpha)/(len(xi) - 3)
+param, param_cov = curve_fit(test4, xi, y)
+chi[2] = chisq(y, test4(xi, param[0], param[1], param[2], param[3]), alpha)/(len(xi) - 4)
+param, param_cov = curve_fit(test5, xi, y)
+chi[3] = chisq(y, test5(xi, param[0], param[1], param[2], param[3], param[4]), alpha)/(len(xi) - 5)
+param, param_cov = curve_fit(test6, xi, y)
+chi[4] = chisq(y, test6(xi, param[0], param[1], param[2], param[3], param[4], param[5]), alpha)/(len(xi) - 6)
+param, param_cov = curve_fit(test7, xi, y)
+chi[5] = chisq(y, test7(xi, param[0], param[1], param[2], param[3], param[4], param[5], param[6]), alpha)/(len(xi) - 7)
+
+for i in range(0, 5):
+    rchi[i] = stats.distributions.chi2.sf(chi[i], len(xi) - i+2)
+
+print(chi, rchi)
+
+
--- main.py
+++ main.py
@@ -5,19 +5,30 @@ def chisq(obs, exp, error):
     return np.sum((obs - exp) ** 2 / (error ** 2))
 
 
-def test(x, a, b, c, d, e, f, g, grad):
-    if grad == 0:
-        return a
-    if grad == 1:
-        return a*x + b
-    if grad == 2:
-        return a*x**2 + b*x + c
-    if grad == 3:
-        return a*x**3 + b*x**2 + c*x + d
-    if grad == 4:
-        return a*x**4 + b*x**3 + c*x**2 + d*x + e
-    if grad == 5:
-        return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
-    if grad == 6:
-        return a*x**6 + b*x**5 + c*x**4 + d*x**3 + e*x**2 + f*x + g
+def test1(x, a):
+    return a
+
+
+def test2(x, a, b):
+    return a*x + b
+
+
+def test3(x, a, b, c):
+    return a*x**2 + b*x + c
+
+
+def test4(x, a, b, c, d):
+    return a*x**3 + b*x**2 + c*x + d
+
+
+def test5(x, a, b, c, d, e):
+    return a*x + b*x**3 + c*x**2 + d*x + e
+
+
+def test6(x, a, b, c, d, e, f):
+    return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
+
+
+def test7(x, a, b, c, d, e, f, g):
+    return a*x**6 + b*x**5 + c*x**4 + d*x**3 + e*x**2 + f*x + g
 
